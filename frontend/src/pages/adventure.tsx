2e34435343edce246647246961c4b1ed13295b27
stefanyralvCVC <stefanyramosalvis@gmail.com>
2025-07-05 16:04:18 -0500

    level bar fixed

--- frontend/src/pages/adventure.tsx
+++ frontend/src/pages/adventure.tsx
@@ -133,6 +133,9 @@ const AdventurePage: React.FC = () => {
   };
 
   const handleLevelComplete = () => {
+    // Agregar el nivel actual a los completados
+    setCompletedLevels(prev => new Set(Array.from(prev).concat(currentLevel)));
+    
     if (currentLevel < dbLevels.length) {
       setCurrentLevel(currentLevel + 1);
     } else {
