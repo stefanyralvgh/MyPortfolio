b440856a5f339ddf4d6df83c72b1f49378234800
stefanyralvCVC <stefany@continentalvc.com>
2025-07-10 11:08:16 -0500

    added cors domains

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:3001", "http://127.0.0.1:3001", "http://localhost:4000", "http://127.0.0.1:4000"
+    origins "http://localhost:3000", "http://127.0.0.1:3000",
+            "http://localhost:3001", "http://127.0.0.1:3001",
+            "http://localhost:4000", "http://127.0.0.1:4000",
+            "https://stefralv.dev", "https://www.stefralv.dev",
+            "https://686fe1032d20780008f4a16b--vermillion-kitten-349ade.netlify.app"
 
     resource "*",
       headers: :any,
