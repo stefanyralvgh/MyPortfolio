b16d6566b506cdd2afc63498760c7c0d1422b13a
Stefany <stefanyramosalvis@gmail.com>
2025-12-23 22:18:59 -0500

    fixed mixed services

--- backend/app/mailers/bug_report_mailer.rb
+++ backend/app/mailers/bug_report_mailer.rb
@@ -1,17 +1,18 @@
 class BugReportMailer < ApplicationMailer
-    def report_email(description:, email:, url:, user_agent:, timestamp:)
-      @description = description
-      @email = email
-      @url = url
-      @user_agent = user_agent
-      @timestamp = timestamp
-  
-      mail(
-        to: ENV.fetch("BUG_REPORT_EMAIL"),
-        from: "Bug Report <stefanyramosalvis+bugs@gmail.com>",
-        reply_to: email,
-        subject: "🐛 New Bug Report"
-      )
-    end
+  def report_email(description:, email:, url:, user_agent:, timestamp:)
+    @description = description
+    @email = email
+    @url = url
+    @user_agent = user_agent
+    @timestamp = timestamp
+
+    mail(
+      to: ENV.fetch("BUG_REPORT_EMAIL"),  
+      from: "stefanyramosalvis+bugs@gmail.com", 
+      reply_to: email, 
+      subject: "🐛 New Bug Report"
+    )
   end
-  
+end
+
+
